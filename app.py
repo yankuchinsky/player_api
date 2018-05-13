07a2fa910443abd66108cb4c9310dd9be5a8e1c8
yankuchinsky <yankuchinsky@yandex.ru>
2018-05-13 22:48:28 +0300

    helpers created

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_sqlalchemy import SQLAlchemy
 from config import username as db_user, password as db_password
+import hashlib
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%s:%s@localhost/Player' %(db_user, db_password)
@@ -17,23 +19,42 @@ class Songs(db.Model):
 	def __init__(self, song_name):
 		self.song_name = song_name
 
-class Login(Resource):
-	def get(self):
-		return {'okey': 'login'}
+class LoginRoute(Resource):
 	def post(self):
 		parser = reqparse.RequestParser()
 		parser.add_argument('name')
 		parser.add_argument('password')
 		args = parser.parse_args()
-		print(args)
+		
 		return {'data': 'null'}
 
-class User(Resource):
+class UserRoute(Resource):
 	def get(self, user_id):
 		return {'username': 'user'}
 	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('user_name')
+		parser.add_argument('user_password')
+		args = parser.parse_args()
+		user_name = args.user_name
+		user_password = args.user_password
+		salt = rand(10)
+		#crypt_password = crypt.crypt(user_password, '$6$' + salt + '$')
+		print(hashlib.sha256(user_password.encode('utf8')).hexdigest())
 		return {'user': 'update'}
 
+class CreateUser(Resouse):
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('user_name')
+		parser.add_argument('user_password')
+		args = parser.parse_args()
+		user_name = args.user_name
+		user_password = args.user_password
+		hashed_password = hashlib.sha256(user_password.encode('utf8')).hexdigest()
+		#crypt_password = crypt.crypt(user_password, '$6$' + salt + '$')
+		print()
+		return{'okey': 'okey'}
 class SongsRoute(Resource):
 	def get(self):
 		songs = Songs.query.all()
@@ -59,7 +80,7 @@ class SongRoute(Resource):
 		else:
 			return{'error': 'No song with that id'}
 
-api.add_resource(Login, '/login')
+api.add_resource(UserRoute, '/login')
 api.add_resource(SongsRoute, '/songs')
 api.add_resource(SongRoute, '/songs/<song_id>')
 
